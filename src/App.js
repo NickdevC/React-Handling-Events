c33aefd06f02021fa98fe512b3cd01ff4a804b15
NickdevC <nick_coles@msn.com>
2023-03-18 12:13:04 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import EventClass from "./components/EventsClass";
+import EventsFunctional from "./components/EventsFunctional";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <EventsFunctional />
+    <EventClass />
+  </div>;
 }
 
 export default App;
